c518bd8a4e23637945ba9743d602733b7720a1ad
MrReese0 <rsengup7@asu.edu>
2020-06-23 02:43:57 -0700

    First Upload

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//import MiniCalendar from "components/MiniCalendar/MiniCalendar";
+import FullCalendar from "components/FullCalendar/FullCalendar";
+//import "assets/scss/MiniCalendar.scss";
+import "assets/scss/FullCalendar/FullCalendar.scss";
+//import "assets/scss/MiniCalendar/MiniCalendar.scss";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function App() {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    //let date = new Date();
+
+    return (
+            //<MiniCalendar mode="datePicker" selectedDate={date}/>
+            <FullCalendar />
+    );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
