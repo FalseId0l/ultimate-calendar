7a78f01ea1e34b5a2bba980d4d476e0ba8919847
MrReese0 <rsengup7@asu.edu>
2020-06-29 06:16:36 -0700

    Updates

---
+++ precache-manifest.b6d1419bef16ef0893fb6b6ace3d16b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aecdd7fea342bf6e225b1eef00939e6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b217f0d082d7cf4f51d",
+    "url": "/static/css/main.142f4c0e.chunk.css"
+  },
+  {
+    "revision": "1bf80fbb29ab1f600ca6",
+    "url": "/static/js/2.ea4f691b.chunk.js"
+  },
+  {
+    "revision": "22d134a87356516cb9f351e71fce83b5",
+    "url": "/static/js/2.ea4f691b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b217f0d082d7cf4f51d",
+    "url": "/static/js/main.7ea0d8e3.chunk.js"
+  },
+  {
+    "revision": "08690edb7203cb432003",
+    "url": "/static/js/runtime-main.a5f5521d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ultimate-calendar/precache-manifest.08b390f82a304fa1bd848884e33a4257.js"
+  "/precache-manifest.b6d1419bef16ef0893fb6b6ace3d16b4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ultimate-calendar/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
